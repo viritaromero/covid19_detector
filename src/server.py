9e8232d43aeeb460df47fc1a2eb3ace139f7602d
Viridiana Romero <31528909+viritaromero@users.noreply.github.com>
2020-03-18 19:07:46 -0700

    Update server.py

--- src/server.py
+++ src/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/open?id=1Hs7OBsQzV3fVf4PLeM_h2o43JyQIIeKv'
+export_file_name = 'model.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Coronavirus', 'Normal']
 path = Path(__file__).parent
 
 app = Starlette()
