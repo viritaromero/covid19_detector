4d258b1f68e3485041250db0a11274fca7c7078f
Viridiana Romero <31528909+viritaromero@users.noreply.github.com>
2020-03-18 20:02:52 -0700

    Update server.py

--- src/server.py
+++ src/server.py
@@ -3,6 +3,7 @@ import asyncio
 import uvicorn
 from fastai import *
 from fastai.vision import *
+from PIL import Image
 from io import BytesIO
 from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
