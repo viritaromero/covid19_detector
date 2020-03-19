91780e498abdcd62c5f8d50f7b9c509baf952dcf
Viridiana Romero <31528909+viritaromero@users.noreply.github.com>
2020-03-18 19:14:04 -0700

    Update server.py

--- src/server.py
+++ src/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/open?id=1Hs7OBsQzV3fVf4PLeM_h2o43JyQIIeKv'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1Hs7OBsQzV3fVf4PLeM_h2o43JyQIIeKv'
 export_file_name = 'model.pkl'
 
 classes = ['Coronavirus', 'Normal']
